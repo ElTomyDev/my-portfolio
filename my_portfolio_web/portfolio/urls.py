bd46d9ccb643543b129abba1f1dee0a1a22d3fbc
Tomas Franco Gimenes <eltomyprog@gmail.com>
2024-08-22 13:22:34 -0300

    configuracion de varias planillas y mejora en el estilo

--- my_portfolio_web/portfolio/urls.py
+++ my_portfolio_web/portfolio/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.home, name="about"),
-    path("projects/", views.home, name="projects"),
+    path("projects/", views.projects, name="projects"),
     path("contact", views.home, name="contact"),
     
 ]
--- my_portfolio_web/portfolio/views.py
+++ my_portfolio_web/portfolio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "portfolio/index.html")
+
+def projects(request):
+    return render(request, "portfolio/projects.html")
