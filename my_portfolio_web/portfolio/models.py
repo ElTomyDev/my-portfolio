333a3cc9de78aec66e4ea3c33bb921be457a32b9
Tomas Franco Gimenes <eltomyprog@gmail.com>
2024-08-29 23:48:19 -0300

    se creo un estilo para proyect y se modifico el html del mismo

--- my_portfolio_web/portfolio/models.py
+++ my_portfolio_web/portfolio/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class Project(models.Model()):
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=340)
     image = models.ImageField(upload_to='projects/')
     link = models.URLField()
+    
     
     def __str__(self):
         return self.title
